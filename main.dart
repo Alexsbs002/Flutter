683c54a6fd028d63b60ee00e7f9d117bb7bdd549
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2022-05-21 21:47:36 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyFirstApp());
+
+class MyFirstApp extends StatefulWidget {
+  const MyFirstApp({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() {
+    return _MyFirstAppState();
+  }
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading = false;
+  double _progressValue = 0.0;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0.0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: const Text("To Do List"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(value: _progressValue),
+                      Text(
+                        '${(_progressValue * 100).round()}%',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ],
+                  )
+                : Text(
+                    "Press button to download",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
+          child: Icon(Icons.cloud_download),
+        ),
+      ),
+    );
+  }
+
+  void _updateProgress() {
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.2;
+
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
+  }
+}
