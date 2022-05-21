7ff23983118fed79930c100acc7a03b13b99ad0d
Alexsbs002 <79782875+Alexsbs002@users.noreply.github.com>
2022-05-21 22:20:22 +0600

    изображение на фоне

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Adding Assets'),
+        ),
+        body: Center(
+            child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Image(
+              image: AssetImage('assets/images/bg.jpg'),
+            ),
+            Image.asset('assets/icons/icon.png')
+          ],
+        )),
+      ),
+    );
+  }
+}
